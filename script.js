78c379e160b3393f1578a1ab146dd9cd0d5b320b
deschew0612 <desireechew.0612@gmail.com>
2024-05-11 08:52:28 +0000

    v1 that works

---
+++ script.js
@@ -0,0 +1,5 @@
+function plusPlus(){
+    document.getElementById("result").innerHTML = document.getElementById("price").value * 1.1 * 1.09;
+}
+
+//TODO: round to 2dp, no need press plus plus for the real price to show, can add numbers in the input box
